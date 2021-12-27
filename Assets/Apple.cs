449e86479d341ca975bafb2a0f3c4fd556141bf3
Nikita Titov <nikov.first@gmail.com>
2021-12-27 19:19:57 +0300

    Upload project

---
+++ Assets/Apple.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour {
+	public static float bottomY = -20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < bottomY) {
+			Destroy (this.gameObject);
+
+			// Получить ссылку на компонент ApplePicker главной камеры Main Camera
+			Apple_Picker apScript = Camera.main.GetComponent<Apple_Picker>();// b
+			// Вызвать общедоступный метод AppleDestroyed() из apScript
+			apScript.AppleDestroyed();
+		}
+	}
+}
